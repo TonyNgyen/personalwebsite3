b425426d4432d0eeaf047644c4b28bfac59ee6fb
Tony Nguyen <tonynguyeenn@gmail.com>
2025-07-03 23:26:24 -0700

    Starting work

--- src/app/blogs/summerPlans/page.tsx
+++ src/app/blogs/summerPlans/page.tsx
@@ -1,7 +1,8 @@
+import BlogContainer from "@/components/BlogComponents/BlogContainer";
 import React from "react";
 
 function Page() {
-  return <div>Page</div>;
+  return <BlogContainer></BlogContainer>;
 }
 
 export default Page;
