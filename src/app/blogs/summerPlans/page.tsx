2b8f7a4ed2f3046c50eded2d452658885b118157
Tony Nguyen <tonynguyeenn@gmail.com>
2025-07-21 14:12:36 -0700

    Updating for deployment

--- src/app/blogs/summerPlans/page.tsx
+++ src/app/blogs/summerPlans/page.tsx
@@ -10,8 +10,8 @@ function Page() {
         <BlogHeader date="July 21, 2025">Summer Break And Plans</BlogHeader>
       </div>
       <BlogSection>
-        It's been about a month of summer so far, and I've been busy with some
-        projects, a job, and a potential start up! I haven't had much time to
+        It&lsquo;s been about a month of summer so far, and I&lsquo;ve been busy with some
+        projects, a job, and a potential start up! I haven&lsquo;t had much time to
         write blogs or notes yet, but I plan to catch up on that soon. And it
         starts with this blog!
       </BlogSection>
@@ -21,7 +21,7 @@ function Page() {
         planning app with recurring tasks and a calendar view. I do plan on
         using it myself, but I am not sure if I can release to the public since
         I believe I need to pay for some of the AWS services I want to use. I
-        need to do more research about that. But other than that, I haven't had
+        need to do more research about that. But other than that, I haven&lsquo;t had
         much time to work on it since I have been busy with another thing.
       </BlogSection>
       <BlogSection>
@@ -32,14 +32,14 @@ function Page() {
         soon. I will keep you updated on that!
       </BlogSection>
       <BlogSection>
-        Finally... I've been working at a coffee/boba shop! It's been an okay
+        Finally... I&lsquo;ve been working at a coffee/boba shop! It&lsquo;s been an okay
         experience so far to be honest, but I do enjoy the people I work with. I
         am mostly using it as a way to make some money while I work on my
         projects and pay off my tuition and rent for the next school year since
         graduate students do not get financial aid...
       </BlogSection>
       <BlogSection>
-        That's about it for now! I will try to write more blogs and notes
+        That&lsquo;s about it for now! I will try to write more blogs and notes
         about my projects and experiences this summer. I have a lot of ideas
         that I want to work on, and I hope to share them with you all soon!
       </BlogSection>
