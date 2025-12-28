09eda8b72e3c272aaaa54660755ff4d56176ac68
Tony Nguyen <tonynguyeenn@gmail.com>
2025-12-27 19:47:20 -0800

    Fixing for mobile UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,13 +21,14 @@ export default async function RootLayout({
     <>
       <html lang="en">
         <head />
-        <body className="flex min-h-screen">
+        <body className="flex min-h-screen overflow-x-hidden">
           <ThemeProvider>
             <Sidebar isLoggedIn={isLoggedIn} />
             <main
               className="md:ml-72 flex-1 py-24 px-4 sm:px-8 md:px-16 lg:px-24 xl:px-48 2xl:px-96
           bg-[radial-gradient(#E7E7E7_1px,transparent_2px)]
-          [background-size:40px_40px] bg-[#fbfbfc] dark:bg-gray-900 dark:bg-[radial-gradient(#292929_1px,transparent_2px)]"
+          [background-size:40px_40px] bg-[#fbfbfc] dark:bg-gray-900 dark:bg-[radial-gradient(#292929_1px,transparent_2px)]
+          w-full max-w-full"
             >
               {children}
             </main>
