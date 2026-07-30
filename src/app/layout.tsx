9a7ba783114e76ca62ff066d094fd4ffceff632b
TonyNgyen <tonynguyeenn@gmail.com>
2026-07-29 17:23:35 -0700

    Bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default async function RootLayout({
   const cookieStore = await cookies();
   const isLoggedIn = cookieStore.get("admin-auth")?.value === "true";
 
-  const BASE_CONTAINER_CSS = `inset-0 -z-10 bg-[size:6rem_4rem] md:ml-72 flex-1 py-24 px-4 sm:px-8 md:px-16 lg:px-24 xl:px-48 2xl:px-96 w-full max-w-full `;
+  const BASE_CONTAINER_CSS = `inset-0 bg-[size:6rem_4rem] md:ml-72 flex-1 py-24 px-4 sm:px-8 md:px-16 lg:px-24 xl:px-48 2xl:px-96 w-full max-w-full `;
   const LIGHT_BACKGROUND_CSS = `bg-[#FAF8F5] bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] `;
   const DARK_BACKGROUND_CSS = `dark:bg-[#1e212bcc] dark:bg-[linear-gradient(to_right,#2a2a2a_1px,transparent_1px),linear-gradient(to_bottom,#2a2a2a_1px,transparent_1px)] `;
 
