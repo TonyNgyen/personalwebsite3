2cbcc82fd84412d38345bed00b8ecaae4ed6b3d1
TonyNgyen <tonynguyeenn@gmail.com>
2026-07-29 17:14:06 -0700

    Update background style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,10 @@ export default async function RootLayout({
   const cookieStore = await cookies();
   const isLoggedIn = cookieStore.get("admin-auth")?.value === "true";
 
+  const BASE_CONTAINER_CSS = `inset-0 -z-10 bg-[size:6rem_4rem] md:ml-72 flex-1 py-24 px-4 sm:px-8 md:px-16 lg:px-24 xl:px-48 2xl:px-96 w-full max-w-full `;
+  const LIGHT_BACKGROUND_CSS = `bg-[#FAF8F5] bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] `;
+  const DARK_BACKGROUND_CSS = `dark:bg-[#1e212bcc] dark:bg-[linear-gradient(to_right,#2a2a2a_1px,transparent_1px),linear-gradient(to_bottom,#2a2a2a_1px,transparent_1px)] `;
+
   return (
     <>
       <html lang="en">
@@ -25,10 +29,9 @@ export default async function RootLayout({
           <ThemeProvider>
             <Sidebar isLoggedIn={isLoggedIn} />
             <main
-              className="md:ml-72 flex-1 py-24 px-4 sm:px-8 md:px-16 lg:px-24 xl:px-48 2xl:px-96
-          bg-[radial-gradient(#E7E7E7_1px,transparent_2px)]
-          [background-size:40px_40px] bg-[#FAF8F5] dark:bg-[#1e212bcc] dark:bg-[radial-gradient(#292929_1px,transparent_2px)]
-          w-full max-w-full"
+              className={
+                BASE_CONTAINER_CSS + LIGHT_BACKGROUND_CSS + DARK_BACKGROUND_CSS
+              }
             >
               {children}
             </main>
